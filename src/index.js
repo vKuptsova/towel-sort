8151062dcafb923b2dbf337a40ca65c3aca8017e
vKuptsova <kuptsowa.vika@gmail.com>
2022-08-06 00:10:49 +0300

    feat: add code to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined) {
+    return [];
+  } else {
+       for (let i = 0; i<matrix.length; i++) {
+        if (i % 2 === 0) {
+          matrix[i] = matrix[i];
+        } else {
+          matrix[i] = matrix[i].sort((a, b) => b - a);
+        }
+       }
+       return matrix.flat();
+      }
 }
